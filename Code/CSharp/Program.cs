6b57a20ae8559df2e1956f47fbbbd076735473f7
vukilelanga <u14035449@tuks.co.za>
2016-08-07 21:56:00 +0200

    Adding Unit testing code
    Adding Unit testing code for FileReader and CommaTokeniser

---
+++ Code/CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityProvider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReaderTests tester = new FileReaderTests();
+            tester.TestGetLines();
+            Console.WriteLine("\nEnd of FileReader Test.\nBeginning Tokeniser Test\n\n");
+            tester.TestTokenise();
+            Console.ReadLine();
+        }
+    }
+}
