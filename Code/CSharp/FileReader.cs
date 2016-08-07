3abba2865278d5738e5eba4ac62836804b797817
vukilelanga <u14035449@tuks.co.za>
2016-08-07 20:33:25 +0200

    Created FileReader.cs
    The C# version of the FileReader.cpp

---
+++ Code/CSharp/FileReader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityProvider
+{
+    abstract class FileReader
+    {
+        public FileReader()
+        {
+        }
+
+        public FileReader(string path)
+        {
+            file = new System.IO.StreamReader(path);
+        }
+
+        public List<string> getLines(string fileName)
+        {
+            if (fileName == null) throw new NullReferenceException();
+
+            List<string> toBeReturned = new List<string>();
+            string currentLine = "";
+            if (file == null) file = new System.IO.StreamReader(fileName);
+
+            do
+            {
+                currentLine = file.ReadLine();
+                if (currentLine != null) toBeReturned.Add(currentLine);
+            } while (currentLine != null);
+
+            file.Close();
+
+            return toBeReturned;
+        }
+
+        System.IO.StreamReader file;
+    }
+}
