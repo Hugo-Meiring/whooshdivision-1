49ff726a30ede257751b3d1b580588a16db42033
vukilelanga <u14035449@tuks.co.za>
2016-08-07 20:35:49 +0200

    Created Tokeniser.cs and CommaTokeniser.cs
    Abstract Tokeniser and concrete CommaTokeniser

---
+++ Code/CSharp/Tokeniser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityProvider
+{
+    abstract class Tokeniser
+    {
+        public abstract string[] tokenise(string line);
+
+        protected char separatorCharacter;
+    }
+}
