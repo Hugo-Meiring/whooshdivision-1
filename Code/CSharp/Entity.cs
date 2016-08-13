f74bb4575444d771e028d2268968d670d3dad00c
Hlatshwayo <u14414555@tuks.co.za>
2016-08-13 08:13:48 +0200

    Update Entity.cs

--- Code/CSharp/Entity.cs
+++ Code/CSharp/Entity.cs
@@ -7,5 +7,34 @@ namespace EntityProvider
 		private String name;
 		private Entity parent;
 		private GameObject obj;
+		
+		public void setName(String name)
+		{
+		     this.name = name;		
+		}
+		
+		public void setGameObject(GameObject obj)
+		{
+			this.obj = obj;
+		}
+		
+		public void setParent(Entity parent)
+		{
+			this.parent = parent;
+		}
+		
+		public String getName()
+		{
+			return this.name;
+		}
+		
+		public GameObject getGameObject()
+		{
+			return this.obj;
+		}
+		public Entity getParent()
+		{
+			return this.parent;
+		}
 	}
 }
