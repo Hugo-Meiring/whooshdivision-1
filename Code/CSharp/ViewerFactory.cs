459f977fae525fb3f21d85ca729459865ee87db5
vukilelanga <u14035449@tuks.co.za>
2016-08-16 21:11:36 +0200

    Created Colour and ViewerFactory classes
    Colour: It's simpler to have a class that builds a colour component based on a hexadecimal than always having to covert the hex colour.
    
    ViewerFactory: to create the viewer object.

---
+++ Code/CSharp/ViewerFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace EntityProvider
+{
+    class ViewerFactory: EntityFactory
+    {
+        public override GameObject build(string[] list)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
