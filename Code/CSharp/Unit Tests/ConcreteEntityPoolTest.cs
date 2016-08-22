0088b80edb215b6dc38bc00f43c1a70f04f2f7ae
Gerome Schutte <gerome.schutte@hotmail.com>
2016-08-22 11:24:28 +0200

    Added indexOf_notFound_ReturnsInvalidIndex test method
    Method tests whether or not ConcreteEntityPool.indexOf returns an index of -1 when it is called with an entity name argument that indicates an entity which is not present in the EntityPool.

--- Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
+++ Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
@@ -7,6 +7,15 @@ namespace EntityProviderTests
     [TestClass]
     public class ConcreteEntityPoolTest
     {
-        
+        [TestMethod]
+        public void indexOf_NotFound_ReturnsInvalidIndex()
+        {
+            int expected = -1;
+
+            ConcreteEntityPool ce_pool = new ConcreteEntityPool();
+            int actual = ce_pool.indexOf("notfound");
+
+            Assert.areEqual(expected, actual, "Index of -1 is not returned when a nonexistent entity is searched for.");
+        }
     }
 }
