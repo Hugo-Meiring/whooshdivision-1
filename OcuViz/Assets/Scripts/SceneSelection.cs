865946486f04b18d450fcab1d44a135924df17c1
Hugo-Meiring <wh.meiring@gmail.com>
2016-08-22 12:45:42 +0200

    Reorganized folder structure + Added Prefabs

---
+++ OcuViz/Assets/Scripts/SceneSelection.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneSelection : MonoBehaviour {
+
+	public Button button;
+	public string sceneName;
+
+	void Start()
+	{
+		button = GetComponent<Button>();
+		button.onClick.AddListener(() => LoadEmptyScene());
+	}
+
+	public void LoadEmptyScene() 
+	{
+		Debug.Log ("Clicked");
+		SceneManager.LoadScene ("EmptyScene");
+	}
+}
