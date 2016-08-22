046a014c17dc3015fa9feac01329c63fe5baec99
Gerome Schutte <gerome.schutte@hotmail.com>
2016-08-22 11:49:39 +0200

    Added xml documentation to ConcreteEntityPoolTest
    Documented the ConcreteEntityPoolTest unit test class using the style specified at: https://msdn.microsoft.com/en-us/library/5ast78ax(v=vs.100).aspx

--- Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
+++ Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
@@ -4,9 +4,18 @@ using EntityProvider;
 
 namespace EntityProviderTests
 {
+    /// <summary>
+    /// A test class which contains unit tests to assess the correctness of the services provided by ConcreteEntityPool.
+    /// <seealso cref="ConcreteEntityPool"/>
+    /// </summary>
     [TestClass]
     public class ConcreteEntityPoolTest
     {
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.indexOf returns a not-found index when calling it with an
+        /// argument which indicates an entity which is not present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.indexOf"/>
+        /// </summary>
         [TestMethod]
         public void indexOf_NotFound_ReturnsInvalidIndex()
         {
@@ -18,6 +27,11 @@ namespace EntityProviderTests
             Assert.areEqual(expected, actual, "Index of -1 is not returned when a nonexistent entity is searched for.");
         }
 
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.indexOf returns a valid index when calling it with an
+        /// argument which indicates an entity which is present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.indexOf"/>
+        /// </summary>
         [TestMethod]
         public void indexOf_Found_ReturnsValidIndex()
         {
@@ -33,6 +47,11 @@ namespace EntityProviderTests
             Assert.areEqual(expected, actual, "Correct index is not returned when a present entity is searched for.");
         }
 
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.store correctly adds an entity to the pool when calling it with an
+        /// argument  entity which is not present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.store"/>
+        /// </summary>
         [TestMethod]
         public void store_NoDuplicates_StoresEntity()
         {
@@ -48,6 +67,12 @@ namespace EntityProviderTests
             Assert.areEqual(expected, actual, "Entity is not added to the pool despite not being a duplicate.");
         }
 
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.store correctly raises an exception when calling it with an
+        /// argument  entity which is present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.store"/>
+        /// <seealso cref="DuplicateEntityException"/>
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(DuplicateEntityException))]
         public void store_Duplicates_ThrowsException()
@@ -61,6 +86,11 @@ namespace EntityProviderTests
             ce_pool.store(bar);
         }
 
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.fetch correctly fetches an entity reference when calling it with an
+        /// argument entity name which indicates an entity that is present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.fetch"/>
+        /// </summary>
         [TestMethod]
         public void fetch_EntityExists_FetchesCorrectReference()
         {
@@ -74,6 +104,12 @@ namespace EntityProviderTests
             Assert.areEqual(foo, actual, "Entity is not fetched despite being present.");
         }
 
+        /// <summary>
+        /// A unit test which checks whether ConcreteEntityPool.fetch correctly raises an exception when calling it with an
+        /// argument entity name which indicates an entity that is not present in the EntityPool
+        /// <seealso cref="ConcreteEntityPool.fetch"/>
+        /// <seealso cref="EntityNotFoundException"/>
+        /// </summary>
         [TestMethod]
         [ExpectedException(typeof(EntityNotFoundException))]
         public void fetch_EntityNotExist_ThrowsException()
