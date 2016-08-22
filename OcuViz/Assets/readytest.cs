3a11a040165d30cc97ec7c04ecccc5319e148161
Hugo-Meiring <wh.meiring@gmail.com>
2016-08-22 09:42:13 +0200

    Mini development changes
    + Added folder structure
    + Added UI scene
    + Added first person controller
    + Converted projected to VCS friendly format
    + Removed VR inappropriate features

---
+++ OcuViz/Assets/readytest.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class readytest : MonoBehaviour {
+
+	public void ChangeScene(string sceneName)
+	{
+		SceneManager.LoadScene (sceneName);
+	}
+}
