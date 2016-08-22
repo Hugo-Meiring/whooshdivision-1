6d0bfa672c3423f307cdcf57ac2e2348cf9a3b2e
Gerome Schutte <gerome.schutte@hotmail.com>
2016-08-22 09:43:11 +0200

    Added exception class for duplicate entities
    If an entity is generated which is already present in the entity pool, an exception of this type must be raised.

---
+++ Code/CSharp/DuplicateEntityException.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace EntityProvider
+{
+    public class DuplicateEntityException: Exception
+    {
+        public DuplicateEntityException()
+        {
+        }
+
+        public DuplicateEntityException(string message)
+            : base(message)
+        {
+        }
+
+        public DuplicateEntityException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }    
+}
