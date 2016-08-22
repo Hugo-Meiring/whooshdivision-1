d64ec9bae1a9ee00ee69c9b0f58bef872a1f5689
Gerome Schutte <gerome.schutte@hotmail.com>
2016-08-22 11:26:13 +0200

    Added indexOf_Found_ReturnsValidIndex test method
    Method tests whether or not ConcreteEntityPool.indexOf returns an index > 0 when it is called with an entity name argument that indicates an entity which is present in the EntityPool.

--- Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
+++ Code/CSharp/Unit Tests/ConcreteEntityPoolTest.cs
@@ -17,5 +17,20 @@ namespace EntityProviderTests
 
             Assert.areEqual(expected, actual, "Index of -1 is not returned when a nonexistent entity is searched for.");
         }
+
+        [TestMethod]
+        public void indexOf_Found_ReturnsValidIndex()
+        {
+            int expected = 0;
+
+            ConcreteEntityPool ce_pool = new ConcreteEntityPool();
+            Entity foo = new Entity();
+            foo.setname("found");
+            ce_pool.store(foo);
+
+            int actual = ce_pool.indexOf("foo");
+
+            Assert.areEqual(expected, actual, "Correct index is not returned when a present entity is searched for.");
+        }
     }
 }
