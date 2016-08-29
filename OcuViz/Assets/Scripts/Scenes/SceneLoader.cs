1d46f21a19ff324fed3eff0dc968f09df3f1679e
vukilelanga <u14035449@tuks.co.za>
2016-08-29 22:48:54 +0200

    Added SceneLoader file
    This is the file used to select the scene to render

---
+++ OcuViz/Assets/Scripts/Scenes/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using EntityProvider;
+
+public class SceneLoader: MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject loadingImage;
+	public void onClick() {
+	    if(SceneManager.GetActiveScene().name == "main")
+        {
+            loadingImage.SetActive(true);
+            SceneManager.LoadScene(1);
+        }
+
+        else if(SceneManager.GetActiveScene().name == "scene")
+        {
+
+        }
+	}
+}
