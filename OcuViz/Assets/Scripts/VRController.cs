b08970674fe5d5b56cca6b554f4cafe55054d290
Hugo-Meiring <wh.meiring@gmail.com>
2016-09-09 11:36:11 +0200

    Added .gitignore back for standard projects

---
+++ OcuViz/Assets/Scripts/VRController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VRController : MonoBehaviour {
+    public float forwardSpeed = 8.0f;
+    public float backwardSpeed = 4.0f;
+    public float strifeSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
