314ef379b886b0aecce79faf77efa711bc04bb6b
Hugo-Meiring <wh.meiring@gmail.com>
2016-10-10 05:41:28 +0200

    VR + Leap mega update
    + Fixed Xbox controller
    + Added leap motion integration
    + Added leap interactable menu
    + Added explorer access files
    + Fixed leap motion engine quaterion bug
    + Imported interaction, hands and UI packages as well as Orion package
    + Fixed framerate issues experienced
    + Added interactive "prison"
    + Reorganised scene and added interactive manager
    + Allow user to grap objects low to the ground

---
+++ OcuViz/Assets/Scripts/Explorer/Explorer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explorer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        ShowExplorer("C:/Users/whmei/Documents/whooshdivision/OcuViz/Assets");
+
+    }
+
+    public void ShowExplorer(string itemPath)
+    {
+        itemPath = itemPath.Replace(@"/", @"\");   // explorer doesn't like front slashes
+        System.Diagnostics.Process.Start("explorer.exe", "/select," + itemPath);
+    }
+}
---
+++ OcuViz/Assets/Scripts/Scenes/InitializeScene.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using EntityProvider;
+
+public class InitializeScene : MonoBehaviour {
+    public void openScene1()
+    {
+        if (SceneManager.GetActiveScene().name == "menu")
+        {
+            //loadingImage.SetActive(true);
+            EntityProvider.EntityProvider.sceneNumber = 1;
+            SceneManager.LoadScene(1);
+        }
+
+        else if (SceneManager.GetActiveScene().name == "scene")
+        {
+
+        }
+    }
+
+    public void openScene2()
+    {
+        EntityProvider.EntityProvider.sceneNumber = 2;
+        SceneManager.LoadScene(2);
+    }
+
+    public void openScene3()
+    {
+        //EntityProvider.EntityProvider.sceneNumber = 3;
+        //SceneManager.LoadScene(3);
+    }
+}
